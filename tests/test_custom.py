f8a652c31658c02dc26d046615d53f980f027ab9
loganmw3 <loganmw3@illinois.edu>
2026-04-16 19:07:03 -0500

    loop test

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -23,38 +23,40 @@ async def test_custom(dut):
     mem_length = 4096
     mem = Memory(mem_length)
 
-    # Init
-    m = randint(1, 8)
-    n = randint(1, 8)
-    p = randint(1, 8)
+    t = 5
+    for _ in range(0, t):
+        # Init
+        m = randint(1, 16)
+        n = randint(1, 16)
+        p = randint(1, 16)
 
-    A = gen_matrix(m, n)
-    B = gen_matrix(n, p)
+        A = gen_matrix(m, n)
+        B = gen_matrix(n, p)
 
-    mem.write_array(0x100, A)
-    mem.write_array(0x200, B)
+        mem.write_array(0x100, A)
+        mem.write_array(0x200, B)
 
-    await start_clock(dut)
-    await reset_dut(dut)
+        await start_clock(dut)
+        await reset_dut(dut)
 
-    await run_config(dut, 0, 0x100, m, n)
-    await run_config(dut, 1, 0x200, n, p)
-    await run_config(dut, 2, 0x300, m, p)
+        await run_config(dut, 0, 0x100, m, n)
+        await run_config(dut, 1, 0x200, n, p)
+        await run_config(dut, 2, 0x300, m, p)
 
-    await run_load(dut, 0, mem.memory)
-    await run_load(dut, 1, mem.memory)
+        await run_load(dut, 0, mem.memory)
+        await run_load(dut, 1, mem.memory)
 
-    await run_gemm(dut, 0, 1, 2)
+        await run_gemm(dut, 0, 1, 2)
 
-    await run_store(dut, 2, mem)
+        await run_store(dut, 2, mem)
 
-    dut_C = mem.read_array(0x300, m * p)
-    golden_C = matmul(A, B, m, n, p)
+        dut_C = mem.read_array(0x300, m * p)
+        golden_C = matmul(A, B, m, n, p)
 
-    print("m, n, p =", m, n, p)
-    print("A =", A)
-    print("B =", B)
-    print("DUT C =", dut_C)
-    print("Golden C =", golden_C)
+        print("m, n, p =", m, n, p)
+        print("A =", A)
+        print("B =", B)
+        print("DUT C =", dut_C)
+        print("Golden C =", golden_C)
 
-    assert dut_C == golden_C, f"Mismatch: DUT={dut_C}, GOLDEN={golden_C}"
+        assert dut_C == golden_C, f"Mismatch: DUT={dut_C}, GOLDEN={golden_C}"
