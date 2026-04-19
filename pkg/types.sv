76f5dbe2e2ba4f37d75d5fe7f06074a60c433564
loganmw3 <loganmw3@illinois.edu>
2026-04-18 19:12:50 -0500

    Update types for ISA 2.0

--- pkg/types.sv
+++ pkg/types.sv
@@ -1,10 +1,10 @@
 package types;
 
-  typedef struct packed {
-    logic [7:0]  rows;
-    logic [7:0]  cols;
+typedef struct packed {
     logic [31:0] ptr;
+    logic [9:0]  rows;
+    logic [9:0]  cols;
     logic        valid;
-  } spad_meta_t;
+} spad_meta_t;
 
 endpackage
